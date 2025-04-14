3a80030ef44e9d02c2a2743136fe5835591e0f78
mdxabu <abdullahfakrudeen2020@gmail.com>
2025-04-14 23:09:37 +0530

    Yet to release v0.1.0

--- cmd/version.go
+++ cmd/version.go
@@ -2,18 +2,28 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/spf13/cobra"
 )
 
-var version = "0.1.0"
+var (
+	version   = "0.1.0"
+	commit    = "unknown"
+	buildDate = "unknown"
+)
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Version of the application",
-	Long:  `Version of the application.`,
+	Short: "Version of the bridge-cli application",
+	Long:  `Version of the bridge-cli application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("bridge-cli-" + version)
+		fmt.Println("Bridge CLI version information:")
+		fmt.Printf("- Version:    %s\n", version)
+		fmt.Printf("- Commit:     %s\n", commit)
+		fmt.Printf("- Built on:   %s\n", buildDate)
+		fmt.Printf("- Go version: %s\n", runtime.Version())
+		fmt.Printf("- OS/Arch:    %s/%s\n", runtime.GOOS, runtime.GOARCH)
 	},
 }
 
